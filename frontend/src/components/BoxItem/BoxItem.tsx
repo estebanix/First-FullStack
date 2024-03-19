4f46d8d643c8b2dd0c398757ee6d68faed1e3bc6
estebanix <dakisunshine@gmail.com>
2024-03-19 08:10:51 +0100

    items activation

--- frontend/src/components/BoxItem/BoxItem.tsx
+++ frontend/src/components/BoxItem/BoxItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./BoxItem.module.scss";
 
 interface BoxItemProps {
@@ -9,8 +10,17 @@ interface BoxItemProps {
 }
 
 export const BoxItem: React.FC<BoxItemProps> = ({ act }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className={styles.boxItem_container}>
+    <div
+      className={`${styles.boxItem_container} ${isActive ? styles.active : ""}`}
+      onClick={() => handleClick()}
+    >
       <h3>{act.activity}</h3>
       <p>{act.description}</p>
     </div>
